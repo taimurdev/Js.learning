e2e3c85ef96377b892e0662ada97cdbe227c866d
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-06-11 20:50:49 +0000

    forof and for in loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: 'javascript',
+    py: 'python',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut for ${myObject[key]}`);
+    
+}
+
+// is forIn looop worlk for array?
+
+const myArray = ['flash', 'batman', 'superman']
+
+for (const key in myArray) {
+    console.log(myArray[key]);
+}
